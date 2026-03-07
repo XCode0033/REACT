f54c0bdde1c98e340b543b6a5ceddc74c1677641
Xavier Stone <XCode0033@gmail.com>
2026-03-06 23:59:12 -0500

    commited

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,37 +1,13 @@
-import reactImg from "./assets/react-core-concepts.png";
-
 import { CORE_CONCEPTS } from "./data.js";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Headers/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect(selectedButton) {
+    //selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
+  }
   return (
     <div>
       <Header></Header>
@@ -48,6 +24,21 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("states")}>
+              States
+            </TabButton>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
